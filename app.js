145f8f94438b2d79c14132df5b1b65d076f96f58
nic <nicshackle@gmail.com>
2020-04-17 09:40:21 +0200

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+var moment = require('moment-timezone');
+console.log(moment().format('h:mm:ss a'))
+console.log(moment().tz('America/Indiana/Indianapolis').format('h:mm:ss a'))
+
+var oled = require('rpi-oled');
+var font = require('oled-font-5x7');
+
+var opts = {
+  width: 128,
+  height: 64,
+  address: 0x3C,
+};
+
+var oled = new oled(opts);
+
+oled.clearDisplay(false);
+oled.fillRect(1, 1, 128, 64, 0);
+oled.setCursor(1, 1);
+oled.writeString(font, 1, 'Cats and dogs are really cool animals, you know.', 1, true);
+
 var ms5803 = require('ms5803')
 var sensor = new ms5803(addr = 0x76)
 
@@ -16,6 +36,12 @@ sensor.reset()
           }
           else{
             PT = r
+            oled.fillRect(1, 1, 128, 64, 0);
+            oled.setCursor(1, 1);
+            oled.writeString(font, 1, `${PT.temperature}C ${PT.pressure}hPa`, 1, true);
+            oled.setCursor(1, 20);
+            oled.writeString(font, 1, moment().tz('America/Indiana/Indianapolis').format('h:mma'), 1, true);
+            oled.writeString(font, 1, moment().format('h:mma'), 1, true);
           }
         })
         .catch((error) => {
@@ -36,3 +62,4 @@ app.use(express.static('public'))
 app.get('/pt', (req, res) => {
    res.send(JSON.stringify(PT))
 })
+
